218e658720a5d5555200489a86979ace096b9ef7
zhirkovpetr <zhirkovpetr95@gmail.com>
2021-11-03 18:32:50 +0300

    feat: completed the task 04-copy-directory (Wen, Nov 03, 2021 06:32:50 PM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs/promises');
+const path = require('path');
+const filesFolder = path.resolve(__dirname, 'files');
+const copyFilesFolder = path.join(__dirname, 'files-copy');
 
+async function copyFolder() {
+  try {
+    await fs.mkdir(copyFilesFolder);
+    console.log('Folder created');
+  } catch {
+    console.log('Folder not created');
+  }
+  try {
+    const file = await fs.readdir(filesFolder);
+    if (file.length === 0) {
+      console.log('Files not copied');
+    }
+    for (let i = 0; i < file.length; i++) {
+      await fs.copyFile(__dirname + `/files/${file[i]}`, __dirname + `/files-copy/${file[i]}`);
+    }
+    console.log('Files copied');
+  } catch {
+    console.log('Error while copying files');
+  }
+}
+
+copyFolder();
