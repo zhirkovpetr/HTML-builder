5098ae70b086a31a9f6f6e8d83fed2704dd51eb1
zhirkovpetr <zhirkovpetr95@gmail.com>
2021-11-08 14:48:13 +0300

    fix: refactoring code in 05-merge-styles task (Mon, Nov 08, 2021 02:48:00 PM)

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,24 +3,31 @@ const fs = require('fs');
 const path = require('path');
 
 const stylesFolder = path.join(__dirname, 'styles');
-const distDirectory = path.join(__dirname,'project-dist','bundle.css');
+const distDirectory = path.join(__dirname, 'project-dist', 'bundle.css');
 
-fs.writeFile(distDirectory,'',(err) =>{if(err) throw err;});
+fs.writeFile(distDirectory, '', (err) => {
+  if (err) throw err;
+});
 
 async function copyStyles() {
   const files = await fsPromis.readdir(stylesFolder, {withFileTypes: true});
   files.forEach(item => {
     if (item.isFile() && path.extname(item.name) === '.css') {
-      const pathFile = path.join(stylesFolder,`${item.name}`);
-      fs.readFile(pathFile, 'utf-8', (err, data) => {
-        if (err) throw err;
-        fs.appendFile(distDirectory, data, (err) => {
-          if (err) throw err;
+      const pathFile = path.join(stylesFolder, `${item.name}`);
+      fs.readFile(pathFile, 'utf-8', (error, data) => {
+        if (handleError(error)) return;
+        fs.appendFile(distDirectory, data, (error) => {
+          if (handleError(error)) return;
         });
       }
       );
     }
   });
+}
+
+function handleError(error) {
+  error ? console.log(error) : null;
+  return !!error;
 }
 
 copyStyles();
